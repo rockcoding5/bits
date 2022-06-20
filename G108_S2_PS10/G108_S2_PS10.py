b27603357491cd34ad736568a712fe84fab9e14f
Shiva <sschary555@gmail.com>
2022-06-20 10:27:54 +0530

    Updated the comments

--- G108_S2_PS10/G108_S2_PS10.py
+++ G108_S2_PS10/G108_S2_PS10.py
@@ -3,9 +3,15 @@ import os
 import sys
 
 
-def read_input_file(inputFile):
+def read_input_file(input_file):
+    """
+    This function reads the given input file (input*.txt) and return List with list of Integer entries
+    [start point, checkout time]
+    :param input_file:
+    :return: list[list[]]
+    """
     try:
-        with open(os.path.join(parentFolderPath, inputFile), 'r') as input_file:
+        with open(os.path.join(parentFolderPath, input_file), 'r') as input_file:
             lines = input_file.readlines()
             if len(lines) == 0:
                 print('Input file is Empty! Please check')
@@ -22,114 +28,132 @@ def read_input_file(inputFile):
                     sys.exit()
         return input_list
     except FileNotFoundError:  # Handle the File not found error
-        print(inputFile + ' Input file not found! Please check')
+        print(input_file + ' Input file not found! Please check')
         exit()
     except:  # Handle any other error
-        print(inputFile + ' Cannot Read input file! Please check')
+        print(input_file + ' Cannot Read input file! Please check')
         exit()
 
 
-def cleanup_output_file(outputFile):
-    if os.path.isfile(os.path.join(parentFolderPath, outputFile)):
-        os.remove(os.path.join(parentFolderPath, outputFile))
+def cleanup_output_file(output_file):
+    """
+    This function just clean up the output file (output*.txt) if exists
+    :param output_file:
+    :return: None
+    """
+    if os.path.isfile(os.path.join(parentFolderPath, output_file)):
+        os.remove(os.path.join(parentFolderPath, output_file))
 
 
-def write_output(outputFile, to_write):
+def write_output(output_file, to_write):
     """
     This function just writes the text given to it in the output file (output*.txt)
+    :param output_file:
     :param to_write:
     :return: None
     """
     to_write = 'Minimum Checkout counters required: ' + str(to_write)
     print(to_write)
-    with open(outputFile, 'a+') as outf:
+    with open(output_file, 'a+') as outf:
         outf.write(to_write)
 
 
 def shift_up_with_parent(heap, pos):
-    endpos = len(heap)
-    startpos = pos
-    newitem = heap[pos]
+    """
+    This function to Shift up the Element with Parent
+    :param heap:
+    :param pos:
+    :return: List
+    """
+    end_pos = len(heap)
+    start_pos = pos
+    new_item = heap[pos]
     # Bubble up the smaller child until hitting a leaf.
-    childpos = 2 * pos + 1  # leftmost child position
-    while childpos < endpos:
-        # Set childpos to index of smaller child.
-        rightpos = childpos + 1
-        if rightpos < endpos and not heap[childpos] < heap[rightpos]:
-            childpos = rightpos
+    child_pos = 2 * pos + 1  # leftmost child position
+    while child_pos < end_pos:
+        # Set child_pos to index of smaller child.
+        right_pos = child_pos + 1
+        if right_pos < end_pos and not heap[child_pos] < heap[right_pos]:
+            child_pos = right_pos
         # Move the smaller child up.
-        heap[pos] = heap[childpos]
-        pos = childpos
-        childpos = 2 * pos + 1
-    # The leaf at pos is empty now.  Put newitem there, and bubble it up
+        heap[pos] = heap[child_pos]
+        pos = child_pos
+        child_pos = 2 * pos + 1
+    # The leaf at pos is empty now.  Put new_item there, and bubble it up
     # to its final resting place (by sifting its parents down).
-    heap[pos] = newitem
-    shift_down_with_child(heap, startpos, pos)
+    heap[pos] = new_item
+    shift_down_with_child(heap, start_pos, pos)
 
 
-def shift_down_with_child(heap, startpos, pos):
-    newitem = heap[pos]
+def shift_down_with_child(heap, start_pos, pos):
+    """
+    This function to Shift down the Element with Child
+    :param heap:
+    :param start_pos:
+    :param pos:
+    :return: List
+    """
+    new_item = heap[pos]
     # Follow the path to the root, moving parents down until finding a place
-    # newitem fits.
-    while pos > startpos:
-        parentpos = (pos - 1) >> 1
-        parent = heap[parentpos]
-        if newitem < parent:
+    # new_item fits.
+    while pos > start_pos:
+        parent_pos = (pos - 1) >> 1
+        parent = heap[parent_pos]
+        if new_item < parent:
             heap[pos] = parent
-            pos = parentpos
+            pos = parent_pos
             continue
         break
-    heap[pos] = newitem
+    heap[pos] = new_item
 
 
-# Push item onto heap, maintaining the heap invariant.
-def heappush(heap, item):
+def cust_heap_push(heap, item):
+    """
+    This function to Push item onto heap, maintaining the heap invariant.
+    :param heap:
+    :param item:
+    :return: List
+    """
     heap.append(item)
     shift_down_with_child(heap, 0, len(heap) - 1)
 
 
-# Fast version of a heappush followed by a heappop.
-def heappushpop(heap, item):
+def cust_heap_push_pop(heap, item):
+    """
+    This function to Fast version of a heappush followed by a heappop.
+    :param heap:
+    :param item:
+    :return: item
+    """
     if heap and heap[0] < item:
         item, heap[0] = heap[0], item
         shift_up_with_parent(heap, 0)
     return item
 
 
-def min_meeting_rooms(intervals):
-    # meetings = [[0, 5], [1, 7], [7, 10], [9, 11], [3, 8], [6, 2]]
-    intervals.sort()
-    # meetings = [[0, 5], [1, 7], [3, 8], [6, 2], [7, 10], [9, 11] ]
-    rooms = 0
-    meeting = []
-    for interval in intervals:
-        if not meeting:
-            meeting.append(interval[1])
-            rooms += 1
-        else:
-            if interval[0] >= meeting[0]:
-                # heapq.heappushpop(meeting, interval[1])  # pop the first heap element
-                heappushpop(meeting, interval[1])  # pop the first heap element
-            else:
-                # heapq.heappush(meeting, interval[1])
-                heappush(meeting, interval[1])
-                rooms += 1
-    return rooms
-
-
 def min_counters(intervals):
+    """
+    This function to find minimum checkout counters required
+    :param intervals:
+    :return: int
+    """
+    # Sort the input List dataset - [start point, checkout time]
     intervals.sort()
     counters = 0
-    counter_list = []
+    checkout_counter_list = []
     for interval in intervals:
-        if not counter_list:
-            counter_list.append(interval[1])
+        if not checkout_counter_list:
+            checkout_counter_list.append(interval[1])
+            # Increment Counters by one
             counters += 1
         else:
-            if interval[0] >= counter_list[0]:
-                heappushpop(counter_list, interval[1])  # pop the first heap element
+            if interval[0] >= checkout_counter_list[0]:
+                # Custom Heap Push and Pop logic method
+                cust_heap_push_pop(checkout_counter_list, interval[1])  # pop the first heap element
             else:
-                heappush(counter_list, interval[1])
+                # Custom Heap Push logic method
+                cust_heap_push(checkout_counter_list, interval[1])
+                # Increment Counters by one
                 counters += 1
     return counters
 
@@ -137,23 +161,21 @@ def min_counters(intervals):
 if __name__ == '__main__':
     # Initialize the Paths. Current folder is parent folder
     parentFolderPath = ''
-    inputFileName = 'inputPS10.txt'
-    outputFile = 'outputPS10.txt'
+    input_file_name = 'inputPS10.txt'
+    output_file_name = 'outputPS10.txt'
 
-    # Read the Input file to create a Binary Tree
-    input_file_list = read_input_file(inputFile=inputFileName)
-    print('Input File List : ' + str(input_file_list))
+    # Read the Input file and create a List with list of Integer entries [start point, checkout time]
+    input_file_list = read_input_file(input_file=input_file_name)
+    # print('Input File List : ' + str(input_file_list))
 
     # Clean the Output file if present
-    cleanup_output_file(outputFile=outputFile)
+    cleanup_output_file(output_file=output_file_name)
 
-    # Find minimum meeting rooms required
-    print(min_meeting_rooms(input_file_list))
-
-    # Find minimum counters required
+    # Find minimum checkout counters required
     min_counter_req = min_counters(input_file_list)
-    print(min_counter_req)
+    # print(min_counter_req)
 
-    write_output(outputFile, min_counter_req)
+    # Write the minimum checkout counters required as a file
+    write_output(output_file_name, min_counter_req)
 
     print(f"Program Successfully executed!")
